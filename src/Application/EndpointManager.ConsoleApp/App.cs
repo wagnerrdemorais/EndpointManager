bf7b3c05a8f1ed54e5775d4a2545f291b8c79a6b
wagner rodakevicz de morais <wagnerrdemorais@gmail.com>
2024-04-23 09:56:42 -0300

    Refactoring main menu and adding confirmation on exit application.

--- src/Application/EndpointManager.ConsoleApp/App.cs
+++ src/Application/EndpointManager.ConsoleApp/App.cs
@@ -8,51 +8,55 @@ internal class App
 {
 
     private readonly IEndpointService _endpointService;
+    private readonly Dictionary<string, Action> _availableActions;
     public App(IEndpointService service)
     {
         _endpointService = service;
+        _availableActions = GetAvailableActions();
     }
 
     public void Run(string[] args)
     {
-            MainMenu();
+        MainMenu();
     }
 
     private void MainMenu()
     {
         try
         {
-            var AvailableActions = GetAvailableActions();
+            PrintMainMenuOptions();
 
-            var selectedOption = "";
-
-            var optionsMenuSBuilder = new StringBuilder();
-            optionsMenuSBuilder.AppendLine("Welcome! Please enter the desired option:");
-            optionsMenuSBuilder.AppendLine("------------------------------------------------------");
-            optionsMenuSBuilder.AppendLine("1) - Insert a new endpoint.");
-            optionsMenuSBuilder.AppendLine("2) - Edit an existing endpoint.");
-            optionsMenuSBuilder.AppendLine("3) - Delete an existing endpoint.");
-            optionsMenuSBuilder.AppendLine("4) - List all endpoints.");
-            optionsMenuSBuilder.AppendLine("5) - Find an endpoint by 'Endpoint Serial Number'.");
-            optionsMenuSBuilder.AppendLine("6) - Exit.");
-            optionsMenuSBuilder.AppendLine("------------------------------------------------------");
-            Console.WriteLine(optionsMenuSBuilder.ToString());
-
-            selectedOption = Console.ReadLine();
-            if (AvailableActions.ContainsKey(selectedOption))
+            var selectedOption = Console.ReadLine();
+            if (_availableActions.ContainsKey(selectedOption))
             {
-                AvailableActions[selectedOption].Invoke();
+                _availableActions[selectedOption].Invoke();
             }
             else
             {
                 Console.WriteLine("Invalid option selected!");
                 MainMenu();
             }
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
             MainMenu();
         }
+    }
+
+    private static void PrintMainMenuOptions()
+    {
+        var optionsMenuSBuilder = new StringBuilder();
+        optionsMenuSBuilder.AppendLine("Please enter the desired option:");
+        optionsMenuSBuilder.AppendLine("------------------------------------------------------");
+        optionsMenuSBuilder.AppendLine("1) - Insert a new endpoint.");
+        optionsMenuSBuilder.AppendLine("2) - Edit an existing endpoint.");
+        optionsMenuSBuilder.AppendLine("3) - Delete an existing endpoint.");
+        optionsMenuSBuilder.AppendLine("4) - List all endpoints.");
+        optionsMenuSBuilder.AppendLine("5) - Find an endpoint by 'Endpoint Serial Number'.");
+        optionsMenuSBuilder.AppendLine("6) - Exit.");
+        optionsMenuSBuilder.AppendLine("------------------------------------------------------");
+        Console.WriteLine(optionsMenuSBuilder.ToString());
     }
 
     private void InsertEndpointOption()
@@ -95,34 +99,40 @@ internal class App
     private void ListEndpointsOption()
     {
         foreach (var endpoint in _endpointService.ListEndpoints())
-        {
             Console.WriteLine(endpoint.ToString());
-        }
+
         MainMenu();
     }
 
     private void FindEndpointOption()
     {
         Console.WriteLine("Enter 'Endpoint Serial Number'");
+
         string serialNumber = Console.ReadLine();
         var endpoint = _endpointService.FindEndpoint(serialNumber);
+
         Console.WriteLine("Endpoint found: ");
         Console.WriteLine(endpoint.ToString());
+
         MainMenu();
     }
 
     private void DeleteEndpointOption()
     {
         Console.WriteLine("Enter 'Endpoint Serial Number'");
+
         string serialNumber = Console.ReadLine();
         _endpointService.DeleteEndpoint(serialNumber);
+
         Console.WriteLine("Endpoint Deleted");
+
         MainMenu();
     }
 
     private void EditEndpointOption()
     {
         Console.WriteLine("Enter 'Endpoint Serial Number'");
+
         string serialNumber = Console.ReadLine();
         var endpoint = _endpointService.FindEndpoint(serialNumber);
 
@@ -132,12 +142,22 @@ internal class App
 
         Console.WriteLine("Switch state changed for endpoint:");
         Console.WriteLine(editedEndpoint.ToString());
+
         MainMenu();
     }
 
     private void ExitOption()
     {
-        Environment.Exit(0);
+        Console.WriteLine("Are you sure? Y/N");
+        var option = Console.ReadLine();
+        if (option.ToLower().Equals("y"))
+        {
+            Environment.Exit(0);
+        }else if (!option.ToLower().Equals("n"))
+        {
+            Console.WriteLine("Invalid Option");
+        }
+        MainMenu();
     }
 
     private Dictionary<string, Action> GetAvailableActions()
